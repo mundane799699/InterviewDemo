8a8fa2d93499572234a4eeb72feb818d92c1870f
mundane <799699348@qq.com>
2022-03-21 23:08:21 +0800

    redis

--- src/main/java/com/mundane/interviewdemo/redislock/GoodController.java
+++ src/main/java/com/mundane/interviewdemo/redislock/GoodController.java
@@ -25,8 +25,7 @@ public class GoodController {
 
         String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
         try {
-            Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);
-            stringRedisTemplate.expire(REDIS_LOCK, 10L, TimeUnit.SECONDS);
+            Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value, 10L, TimeUnit.SECONDS);
             if (!flag) {
                 // 加锁失败
                 return "抢锁失败";
@@ -44,7 +43,9 @@ public class GoodController {
 
             return "商品已经售完/活动结束/调用超时,欢迎下次光临" + "\t服务提供端口" + serverPort;
         } finally {
-            stringRedisTemplate.delete(REDIS_LOCK);
+            if (stringRedisTemplate.opsForValue().get(REDIS_LOCK).equalsIgnoreCase(value)) {
+                stringRedisTemplate.delete(REDIS_LOCK);
+            }
         }
     }
 
